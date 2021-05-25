7253c7e17b78e49be736a5053df566f10f42926a
Denzel Code <denzelcodedev@gmail.com>
2021-05-25 15:55:36 -0400

    chore(package.json): package json updated

--- src/config/config.factory.ts
+++ src/config/config.factory.ts
@@ -1,11 +1,8 @@
-import { Dictionary } from 'lodash';
+import { Dictionary } from '../types';
 import { Config } from './config';
 
 export class ConfigFactory {
-  static getConfig<T extends Object = Dictionary<any>>(
-    path: string,
-    defaultValues?: T
-  ): Config<T> {
+  static getConfig<T extends Object = Dictionary<any>>(path: string, defaultValues?: T): Config<T> {
     return new Config<T>(path, defaultValues);
   }
 }
