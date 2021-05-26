a34ba4f0e21c33325850a17228bfcc17736cd6ff
Denzel Code <denzelcodedev@gmail.com>
2021-05-26 01:48:57 -0400

    fix: export name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ConfigFactory } from './config/config.factory';
-export { ConfigInterface as ConfigDefinition } from './config/config.interface';
+export { ConfigInterface } from './config/config.interface';
 export { Config } from './config/config';
 export { Dictionary } from './types';
